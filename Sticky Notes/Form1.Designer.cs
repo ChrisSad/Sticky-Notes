b6deeccb898740cfdb459707b1cf4839e0f2c361
ChrisSad <93772362+ChrisSad@users.noreply.github.com>
2021-12-26 15:38:29 -0500

    Add project files.

---
+++ Sticky Notes/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Sticky_Notes
+{
+    partial class Notepad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Notepad));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.addToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.saveToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.notesRTB = new System.Windows.Forms.RichTextBox();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addToolStripButton,
+            this.saveToolStripButton});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(376, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // addToolStripButton
+            // 
+            this.addToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.addToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("addToolStripButton.Image")));
+            this.addToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.addToolStripButton.Name = "addToolStripButton";
+            this.addToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.addToolStripButton.Text = "&New";
+            this.addToolStripButton.Click += new System.EventHandler(this.addToolStripButton_Click);
+            // 
+            // saveToolStripButton
+            // 
+            this.saveToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.saveToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("saveToolStripButton.Image")));
+            this.saveToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.saveToolStripButton.Name = "saveToolStripButton";
+            this.saveToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.saveToolStripButton.Text = "&Save";
+            this.saveToolStripButton.Click += new System.EventHandler(this.saveToolStripButton_Click);
+            // 
+            // notesRTB
+            // 
+            this.notesRTB.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.notesRTB.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.notesRTB.Location = new System.Drawing.Point(12, 28);
+            this.notesRTB.Name = "notesRTB";
+            this.notesRTB.Size = new System.Drawing.Size(352, 320);
+            this.notesRTB.TabIndex = 1;
+            this.notesRTB.Text = "";
+            // 
+            // Notepad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 360);
+            this.Controls.Add(this.notesRTB);
+            this.Controls.Add(this.toolStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Notepad";
+            this.Text = "Notepad";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton addToolStripButton;
+        private System.Windows.Forms.ToolStripButton saveToolStripButton;
+        private System.Windows.Forms.RichTextBox notesRTB;
+    }
+}
+
---
+++ Sticky Notes/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Sticky_Notes
+{
+    public partial class Notepad : Form
+    {
+        public Notepad()
+        {
+            InitializeComponent();
+
+            retrieveNotes();
+        }
+
+        public void retrieveNotes()
+        {
+            try
+            {
+                string text;
+                using (var sr = new StreamReader("notes.txt"))
+                {
+                    text = sr.ReadToEnd();
+                    
+                }
+                notesRTB.Text = text;
+            }
+            catch (IOException e)
+            {
+                MessageBox.Show("The file could not be read:\n"+e.Message);
+            }
+        }
+
+        private void addToolStripButton_Click(object sender, EventArgs e)//new note button
+        {
+
+        }
+
+        private void saveToolStripButton_Click(object sender, EventArgs e)//save button
+        {
+            try
+            {
+                using (var sr = new StreamWriter("notes.txt"))
+                {
+                    sr.Write(notesRTB.Text);
+
+                }
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show("The file could not be written in:\n" + ex.Message);
+            }
+        }
+
+        
+    }
+}
